8084607421c016d564ced7ad91198d809d1f13a8
Younes Strittmatter <ystrittm@gmail.com>
2024-12-01 01:11:07 -0500

    Update stimulus_sequence.py

--- tutorial/res/stimulus_sequence.py
+++ tutorial/res/stimulus_sequence.py
@@ -1,16 +1,16 @@
-from sweetbean.stimulus import FixationStimulus, BlankStimulus, RandomDotPatternsStimulus
+from sweetbean.stimulus import Fixation, Blank, RandomDotPatterns
 from sweetbean.parameter import TimelineVariable
 from sweetbean.sequence import Block, Experiment
 
-def stimulus_sequence(timeline, intensity_1, intensity_2):
-    fixation = FixationStimulus(800)
-    blank_1 = BlankStimulus(400)
-    blank_2 = BlankStimulus(1000)
-    s1 = TimelineVariable('S1', [40, 70])
-    s2 = TimelineVariable('S2', [40, 70])
+def stimulus_sequence(timeline):
+    fixation = Fixation(800)
+    blank_1 = Blank(400)
+    blank_2 = Blank(1000)
+    s1 = TimelineVariable('S1')
+    s2 = TimelineVariable('S2')
 
 
-    rdp = RandomDotPatternsStimulus(
+    rdp = RandomDotPatterns(
         duration=2000,
         number_of_oobs=[s1, s2],
         number_of_apertures=2,
